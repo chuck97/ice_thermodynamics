86e8cd233152093aa514357cbd8493db22e70225
chuck97 <sergey.petrov@phystech.edu>
2022-08-31 22:44:03 +0300

    deleted unused files

--- Headers/thomas_solver.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include <iostream>
-
-#define ERR(message) {std::cerr << "Error: " << message  << std::endl; exit(1);}
-
-// lhs is vector of 3 diagonals: ld, c, ru
-std::vector<double> thomas_solver(const std::vector<std::vector<double>>& lhs,
-                                  const std::vector<double>& rhs);
